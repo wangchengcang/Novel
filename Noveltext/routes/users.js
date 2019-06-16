c7218f37d48390b066dad05e3710ed9c85032a70
wangchengcang <2322305381@qq.com>
2019-06-16 14:43:28 +0800

    js完善

--- Noveltext/routes/users.js
+++ Noveltext/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs=require('fs');
 var sql=require('./mysql.js');
 var multer=require('multer');
+var url=require('url');
 var querystring=require('querystring');
 var path=require('path');
 var router = express.Router();
@@ -79,7 +80,24 @@ router.post('/set_text',function(req,res){
 		arr:[json.titleimg,json.title,json.name,json.xq,json.songuid],
 		sql:'insert into xs(ximg,xtitle,xname,xcontent,songuid) values(?,?,?,?,?)',
 		success(data){
-			res.send('ok')
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
+//小说章节
+//set_list
+router.post('/set_list',function(req,res){
+	var json=req.body;
+	console.log(json)
+	sql.con({
+		arr:[json.list,json.txt,json.xsuid],
+		sql:'insert into list(ztitle,zcontent,xsuid) values(?,?,?)',
+		success(data){
+			res.send(data)
 		},
 		error(err){
 			res.send(err)
@@ -87,4 +105,98 @@ router.post('/set_text',function(req,res){
 		
 	})
 })	
+//搜索
+//ss
+router.post('/ss',function(req,res){
+	var json=req.body;
+	console.log(json.text)
+	sql.con({
+		arr:[json.text],
+		sql:'select * from xs where xtitle like "%"?"%"',
+		success(data){
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})	
+//热推
+router.post('/rt',function(req,res){
+	sql.con({
+		arr:[],
+		sql:'select * from xs',
+		success(data){
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
+//点击小说查看详情	
+router.post('/xq',function(req,res){
+	var json=req.body;
+	sql.con({
+		arr:[json.uid],
+		sql:'select * from list where xsuid=?',
+		success(data){
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
+//小锁内容
+//concent
+router.post('/concent',function(req,res){
+	var json=req.body;
+	sql.con({
+		arr:[json.uid],
+		sql:'select * from list where uid=?',
+		success(data){
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
+//发表评论
+//comment
+router.post('/comment',function(req,res){
+	var json=req.body;
+	sql.con({
+		arr:[json.username,json.userimg,json.content,json.xsuid,json.useruid],
+		sql:'insert into comment(username,userimg,content,xsuid,useruid) values(?,?,?,?,?)',
+		success(data){
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
+//展示当前小说评论
+//comment_list
+router.post('/comment_list',function(req,res){
+	var json=req.body;
+	sql.con({
+		arr:[json.uid],
+		sql:'select * from comment where xsuid=?',
+		success(data){
+			res.send(data)
+		},
+		error(err){
+			res.send(err)
+		}
+		
+	})
+})
 module.exports = router;
